5a3775e517fff2d3d05830938a4bc702e0c2f376
KauaCruzPereira <kauacruzps@gmail.com>
2026-06-25 16:57:47 -0300

    feat - new books

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -474,124 +474,206 @@ export const BOOKS = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_9YEvR_sU2KNuHO4F3DX-8XNxVS7RLigb4y8SG5xBJw&s=10",
     disciplines: ["Filosofia"],
   },
-  
- {
-  title: "Os Sertões",
-  authors: "Euclides da Cunha",
-  pdfUrl: "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_7b9cdae2b94e467699d0319e05c35acf.pdf",
-     imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2Awi3ZCQEyq2MARGg1E5hM9AxOMPhHuE-W3JLSmB1lQDr5fFM2brUS2s&s=10",
-  disciplines: ["História"],
-},
-{
-  title: "América Latina: Males de Origem",
-  authors: "Manoel Bomfim",
-  pdfUrl: "https://fundar.org.br/wp-content/uploads/2021/06/america-latina-males-de-origem.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/81Yox7+EKgL._UF1000,1000_QL80_.jpg",
-  disciplines: ["Redação"],
-},
-{
-  title: "O Abolicionismo",
-  authors: "Joaquim Nabuco",
-  pdfUrl: "https://camara-leg-br.usrfiles.com/ugd/5ca0e9_aa6e5d298d3443d3a8923d0eb5b58b3a.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTRvABCHe_q1THN7feoxsPGtOWlLPWeUVw7dRM3HlXnQ&s=10",
-  disciplines: ["Redação"],
-},
-{
-  title: "A Origem das Espécies",
-  authors: "Charles Darwin",
-  pdfUrl: "https://domainpublic.wordpress.com/wp-content/uploads/2022/01/darwin.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSitOrruNlpj7UGzWzD1ZnwxTb3uhzYY4ttCuO6jJugDg&s=10",
-  disciplines: ["Redação"],
-},
-{
-  title: "Do Contrato Social",
-  authors: "Jean-Jacques Rousseau",
-  pdfUrl: "https://www.ebooksbrasil.org/adobeebook/contratosocial.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX0Gs4kn8cbps1JfmC-wAAaa1C5Vnf6JJ6YWyw7ZcpXQ&s=10",
-  disciplines: ["Redação"],
-},
-{
-  title: "Cartas Chilenas",
-  authors: "Tomás Antônio Gonzaga",
-  pdfUrl: "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_f45b66f4a4f94f52ad86019271c72574.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/71uYQhWCBZL._AC_UF1000,1000_QL80_.jpg",
-  disciplines: ["Redação"],
-},
-{
-  title: "Elogio da Loucura",
-  authors: "Erasmo de Roterdã",
-  pdfUrl: "https://www.ebooksbrasil.org/adobeebook/erasmo.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScF4g25GKNnM7WVRqvY_oevfwxI7EQeEXRQPa7OcbkLA&s=10",
-  disciplines: ["Redação"],
-},
-{
-  title: "Segundo Tratado Sobre o Governo Civil",
-  authors: "John Locke",
-  pdfUrl: "https://www.xr.pro.br/if/locke-segundo_tratado_sobre_o_governo.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/81fFpc97I8L._UF1000,1000_QL80_.jpg",
-  disciplines: ["Redação"],
-},
-  {
-  title: "As Aventuras de Sherlock Holmes",
-  authors: "Arthur Conan Doyle",
-  pdfUrl: "https://kbook.com.br/wp-content/files_mf/asaventurasdesherlockholmes.pdf",
-       imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfkAObglmOekTDjzRznQdgQ5aqDgUtU7UxZdZ-6CQd7w&s=10",
-  disciplines: ["Matemática"],
-},
 
   {
-  title: "Vinte Mil Léguas Submarinas",
-  authors: "Júlio Verne",
-  pdfUrl: "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_ba904ddcc828485b865fbd0a48a5ac62.pdf",
-       imageUrl:
+    title: "Os Sertões",
+    authors: "Euclides da Cunha",
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_7b9cdae2b94e467699d0319e05c35acf.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2Awi3ZCQEyq2MARGg1E5hM9AxOMPhHuE-W3JLSmB1lQDr5fFM2brUS2s&s=10",
+    disciplines: ["História"],
+  },
+  {
+    title: "América Latina: Males de Origem",
+    authors: "Manoel Bomfim",
+    pdfUrl:
+      "https://fundar.org.br/wp-content/uploads/2021/06/america-latina-males-de-origem.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/81Yox7+EKgL._UF1000,1000_QL80_.jpg",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "O Abolicionismo",
+    authors: "Joaquim Nabuco",
+    pdfUrl:
+      "https://camara-leg-br.usrfiles.com/ugd/5ca0e9_aa6e5d298d3443d3a8923d0eb5b58b3a.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTRvABCHe_q1THN7feoxsPGtOWlLPWeUVw7dRM3HlXnQ&s=10",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "A Origem das Espécies",
+    authors: "Charles Darwin",
+    pdfUrl:
+      "https://domainpublic.wordpress.com/wp-content/uploads/2022/01/darwin.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSitOrruNlpj7UGzWzD1ZnwxTb3uhzYY4ttCuO6jJugDg&s=10",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "Do Contrato Social",
+    authors: "Jean-Jacques Rousseau",
+    pdfUrl: "https://www.ebooksbrasil.org/adobeebook/contratosocial.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX0Gs4kn8cbps1JfmC-wAAaa1C5Vnf6JJ6YWyw7ZcpXQ&s=10",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "Cartas Chilenas",
+    authors: "Tomás Antônio Gonzaga",
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_f45b66f4a4f94f52ad86019271c72574.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71uYQhWCBZL._AC_UF1000,1000_QL80_.jpg",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "Elogio da Loucura",
+    authors: "Erasmo de Roterdã",
+    pdfUrl: "https://www.ebooksbrasil.org/adobeebook/erasmo.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScF4g25GKNnM7WVRqvY_oevfwxI7EQeEXRQPa7OcbkLA&s=10",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "Segundo Tratado Sobre o Governo Civil",
+    authors: "John Locke",
+    pdfUrl:
+      "https://www.xr.pro.br/if/locke-segundo_tratado_sobre_o_governo.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/81fFpc97I8L._UF1000,1000_QL80_.jpg",
+    disciplines: ["Redação"],
+  },
+  {
+    title: "As Aventuras de Sherlock Holmes",
+    authors: "Arthur Conan Doyle",
+    pdfUrl:
+      "https://kbook.com.br/wp-content/files_mf/asaventurasdesherlockholmes.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfkAObglmOekTDjzRznQdgQ5aqDgUtU7UxZdZ-6CQd7w&s=10",
+    disciplines: ["Matemática"],
+  },
+
+  {
+    title: "Vinte Mil Léguas Submarinas",
+    authors: "Júlio Verne",
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_ba904ddcc828485b865fbd0a48a5ac62.pdf",
+    imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfgOcII_kewllkBiVyzFRYvFGEYEB0Em4fjr-GRzPMFw&s=10",
-  disciplines: ["Matemática"],
-},
-{
-  title: "Viagem ao Centro da Terra",
-  authors: "Júlio Verne",
-  pdfUrl: "https://archivepublicdomain.com/files/2025/03/viagem_ao_centro_da_terra_-_julio_verne.pdf?t=f8c6ac3743183cdf9a9d68863baf4406c233a67e3f6d061659b44e73a4c6ee26",
+    disciplines: ["Matemática"],
+  },
+  {
+    title: "Viagem ao Centro da Terra",
+    authors: "Júlio Verne",
+    pdfUrl:
+      "https://archivepublicdomain.com/files/2025/03/viagem_ao_centro_da_terra_-_julio_verne.pdf?t=f8c6ac3743183cdf9a9d68863baf4406c233a67e3f6d061659b44e73a4c6ee26",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN5TMIEq0fjGmqdVY-rLPb9uhq-RT6ZJ25hZUY9SDgmg&s=10",
-  disciplines: ["Matemática"],
-},
-{
-  title: "A Ilha do Dr. Moreau",
-  authors: "H. G. Wells",
-  pdfUrl: "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_4df7e093815b42a4a4e31a043a7980c3.pdf",
-     imageUrl:
+    disciplines: ["Matemática"],
+  },
+  {
+    title: "A Ilha do Dr. Moreau",
+    authors: "H. G. Wells",
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_4df7e093815b42a4a4e31a043a7980c3.pdf",
+    imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq3QD7CqTY9VIrIbLZso1GOJyVXr6oUh1N4UjHpvL9-Q&s=10",
-  disciplines: ["Matemática"],
-},
-{
-  title: "Da Terra à Lua",
-  authors: "Júlio Verne",
-  pdfUrl: "https://archivepublicdomain.com/files/2025/03/Da-Terra-a-Lua-Julio-Verne.pdf?t=334a7f20471ee414e3",
-     imageUrl:
+    disciplines: ["Matemática"],
+  },
+  {
+    title: "Da Terra à Lua",
+    authors: "Júlio Verne",
+    pdfUrl:
+      "https://archivepublicdomain.com/files/2025/03/Da-Terra-a-Lua-Julio-Verne.pdf?t=334a7f20471ee414e3",
+    imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfHLlEZJo2Y0AabrjLVn07QK2_hQDA_lR_qPq1Pn2vTQ&s=10",
-  disciplines: ["Matemática"],
-},
+    disciplines: ["Matemática"],
+  },
   {
-  title: "O Homem que Calculava",
-  authors: "Malba Tahan (pseudônimo de Júlio César de Mello e Souza)",
-  pdfUrl: "https://leituramelhorviagem.wordpress.com/wp-content/uploads/2016/11/o-homem-que-calculava-malba-tahan-1.pdf",
-  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0s3-p1ne1FdShqbXeM5ZEwC0lcDZORgW9KNBX2-W4w&s",
-  disciplines: ["Matemática"],
-},
+    title: "O Homem que Calculava",
+    authors: "Malba Tahan (pseudônimo de Júlio César de Mello e Souza)",
+    pdfUrl:
+      "https://leituramelhorviagem.wordpress.com/wp-content/uploads/2016/11/o-homem-que-calculava-malba-tahan-1.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0s3-p1ne1FdShqbXeM5ZEwC0lcDZORgW9KNBX2-W4w&s",
+    disciplines: ["Matemática"],
+  },
   {
-  title: "Memórias de um Sargento de Milícias",
-  authors: "Manuel Antônio de Almeida",
-  pdfUrl: "https://fundar.org.br/wp-content/uploads/2021/06/memorias-de-um-sargento-de-milicias.pdf",
-  imageUrl: "https://m.media-amazon.com/images/I/71MebPfNmQL._AC_UF1000,1000_QL80_.jpg",
-  disciplines: ["Geografia"],
-},
+    title: "Memórias de um Sargento de Milícias",
+    authors: "Manuel Antônio de Almeida",
+    pdfUrl:
+      "https://fundar.org.br/wp-content/uploads/2021/06/memorias-de-um-sargento-de-milicias.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71MebPfNmQL._AC_UF1000,1000_QL80_.jpg",
+    disciplines: ["Geografia"],
+  },
+  {
+    title: "A Volta ao Mundo em 80 Dias",
+    authors: "Júlio Verne",
+    disciplines: ["Geografia"],
+    pdfUrl:
+      "https://archivepublicdomain.com/files/2025/03/a_volta_ao_mundo_em_80_dias_-_julio_verne.pdf?t=c2a25085a6df78cd834c79c7d89d1d1eaff52893697ea5bc4f1224df032d06e9",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/51891EKxfNL._SY445_SX342_ML2_.jpg",
+  },
+
+  {
+    title: "O Coração das Trevas",
+    authors: "Joseph Conrad",
+    disciplines: ["Geografia"],
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_0f5053815e234cc997dbbe6393ba925e.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/41O6ap88L9L._SY445_SX342_ML2_.jpg",
+  },
+
+  {
+    title: "O Guarani",
+    authors: "José de Alencar",
+    disciplines: ["Geografia"],
+    pdfUrl:
+      "https://archivepublicdomain.com/files/2025/10/o-guarani.pdf?t=e98e2d9a35df1f6348f58db02eb9a96bdadfa0bdb37efa3d967673097ef248dc",
+    imageUrl:
+      "https://s4.static.brasilescola.uol.com.br/be/2023/07/ilustracao-de-um-grupo-de-indigenas-na-capa-do-livro-o-guarani-de-jose-de-alencar.jpg",
+  },
+
+  {
+    title: "Triste Fim de Policarpo Quaresma",
+    authors: "Lima Barreto",
+    disciplines: ["História"],
+    pdfUrl:
+      "https://camara-gov-br.usrfiles.com/ugd/5ca0e9_ebda356b2f4742f3bf3325afd9e44363.pdf",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2juuXHASlvgZ1hOj86wydecCjBl6dlEdS2Q&s",
+  },
+
+  {
+    title: "Os Miseráveis",
+    authors: "Victor Hugo",
+    disciplines: ["História"],
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_877ef06ea4304c21933dff36fbb87935.pdf",
+    imageUrl: "https://m.media-amazon.com/images/I/71L28YvPobL._SY522_.jpg",
+  },
+
+  {
+    title: "Utopia",
+    authors: "Thomas More",
+    disciplines: ["História"],
+    pdfUrl: "https://www.ebooksbrasil.org/adobeebook/utopia.pdf",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/91ZnuoJ-I0L._UF1000,1000_QL80_.jpg",
+  },
+
+  {
+    title: "Memórias de um Sargento de Milícias",
+    authors: "Manuel Antônio de Almeida",
+    disciplines: ["História"],
+    pdfUrl:
+      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_5ad320b0fed546a599ca5d2eb4a4cf1f.pdf",
+    imageUrl:
+      "https://www.baixelivros.com.br/media/2020/03/memorias-de-um-sargento.jpg",
+  },
 ];
