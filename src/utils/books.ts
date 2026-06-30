23f25597843152220f9d7ec41fd27b601a16cc3b
KauaCruzPereira <kauacruzps@gmail.com>
2026-06-30 14:48:21 -0300

    feat - change all books imageUrl for a reliable source

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -4,8 +4,7 @@ export const BOOKS = [
     authors: "Júlia Lopes de Almeida",
     pdfUrl:
       "https://www.epedagogia.com.br/materialbibliotecaonine/261A-falencia.pdf",
-    imageUrl:
-      "https://covers.openlibrary.org/b/id/4250811-L.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/4250811-L.jpg",
     disciplines: ["Língua Portuguesa"],
   },
 
@@ -33,8 +32,7 @@ export const BOOKS = [
     title: "Triste Fim de Policarpo Quaresma",
     authors: "Lima Barreto",
     pdfUrl: "https://www.ebooksbrasil.org/adobeebook/policarpoE.pdf",
-    imageUrl:
-      "https://covers.openlibrary.org/b/id/10653568-L.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/10653568-L.jpg",
     disciplines: ["Língua Portuguesa"],
   },
   {
@@ -42,8 +40,7 @@ export const BOOKS = [
     authors: "Bernardo Guimarães",
     pdfUrl:
       "https://objdigital.bn.br/objdigital2/acervo_digital/div_obrasraras/or43590/or43590.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb-OkyViDe3XCtlIwqYuhGdtjAGQHB7XaIlA&s",
+    imageUrl: "https://covers.openlibrary.org/b/id/8123151-M.jpg",
     disciplines: ["Língua Portuguesa"],
   },
 
@@ -52,8 +49,7 @@ export const BOOKS = [
     authors: "Joaquim Manuel de Macedo",
     pdfUrl:
       "https://objdigital.bn.br/objdigital2/Acervo_Digital/livros_eletronicos/bndigital0017/bndigital0017.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlREVaXfM76lynY0T826I0ybDZ02OdkxZFDx2--mmiRrVukBd1E78Uqw&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/15203760-M.jpg",
     disciplines: ["Língua Portuguesa"],
   },
 
@@ -62,8 +58,7 @@ export const BOOKS = [
     authors: "Machado de Assis",
     pdfUrl:
       "https://www.curso-objetivo.br/vestibular/assets/download/obras-literarias/machado-de-assis/o-alienista.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi5OePHQ11kDRFybJt5VGQiYV6e4At8tumJA&s",
+    imageUrl: "https://covers.openlibrary.org/b/id/11688576-M.jpg",
     disciplines: ["História"],
   },
 
@@ -72,24 +67,15 @@ export const BOOKS = [
     authors: "Inglês de Souza",
     pdfUrl:
       "https://objdigital.bn.br/objdigital2/Acervo_Digital/Livros_eletronicos/O%20missionario.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7yyTXhoAcYEJwxI_SZoFPHXLNUj83k-lCyxM9XZ7gSr6-zlX_UdMqtaE&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/5852840-L.jpg",
     disciplines: ["Língua Portuguesa"],
   },
-  {
-    title: "O Contrato Social",
-    authors: "Jean-Jacques Rousseau",
-    pdfUrl: "https://www.ebooksbrasil.org/adobeebook/contratosocial.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnL3D4y2fenktGrUED1BWKLOcjJ_ilSiOcGixwx9nDBLTOtW0sYsOYjqSV&s=10",
-    disciplines: ["Filosofia"],
-  },
+
   {
     title: "A Sociedade do Espetáculo",
     authors: "Guy Debord",
     pdfUrl: "https://www.marxists.org/portugues/debord/1967/11/sociedade.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJbWsQzLPZbtQaj25jfajYMsHSAhcNe01FPH_3ZvwNlw&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/15207236-M.jpg",
     disciplines: ["Filosofia"],
   },
   {
@@ -97,60 +83,29 @@ export const BOOKS = [
     authors: "Euclides da Cunha",
     pdfUrl:
       "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_7b9cdae2b94e467699d0319e05c35acf.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2Awi3ZCQEyq2MARGg1E5hM9AxOMPhHuE-W3JLSmB1lQDr5fFM2brUS2s&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/13868770-M.jpg",
     disciplines: ["História"],
-  },
-  {
-    title: "América Latina: Males de Origem",
-    authors: "Manoel Bomfim",
-    pdfUrl:
-      "https://fundar.org.br/wp-content/uploads/2021/06/america-latina-males-de-origem.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/81Yox7+EKgL._UF1000,1000_QL80_.jpg",
-    disciplines: ["Redação"],
-  },
-  {
-    title: "O Abolicionismo",
-    authors: "Joaquim Nabuco",
-    pdfUrl:
-      "https://camara-leg-br.usrfiles.com/ugd/5ca0e9_aa6e5d298d3443d3a8923d0eb5b58b3a.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTRvABCHe_q1THN7feoxsPGtOWlLPWeUVw7dRM3HlXnQ&s=10",
-    disciplines: ["Redação"],
   },
   {
     title: "A Origem das Espécies",
     authors: "Charles Darwin",
     pdfUrl:
       "https://domainpublic.wordpress.com/wp-content/uploads/2022/01/darwin.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSitOrruNlpj7UGzWzD1ZnwxTb3uhzYY4ttCuO6jJugDg&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/8539894-M.jpg",
     disciplines: ["Redação"],
   },
   {
     title: "Do Contrato Social",
     authors: "Jean-Jacques Rousseau",
     pdfUrl: "https://www.ebooksbrasil.org/adobeebook/contratosocial.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX0Gs4kn8cbps1JfmC-wAAaa1C5Vnf6JJ6YWyw7ZcpXQ&s=10",
-    disciplines: ["Redação"],
-  },
-  {
-    title: "Cartas Chilenas",
-    authors: "Tomás Antônio Gonzaga",
-    pdfUrl:
-      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_f45b66f4a4f94f52ad86019271c72574.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/71uYQhWCBZL._AC_UF1000,1000_QL80_.jpg",
-    disciplines: ["Redação"],
+    imageUrl: "https://covers.openlibrary.org/b/id/12370626-M.jpg",
+    disciplines: ["Filosofia"],
   },
   {
     title: "Elogio da Loucura",
     authors: "Erasmo de Roterdã",
     pdfUrl: "https://www.ebooksbrasil.org/adobeebook/erasmo.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScF4g25GKNnM7WVRqvY_oevfwxI7EQeEXRQPa7OcbkLA&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/15137102-M.jpg",
     disciplines: ["Redação"],
   },
   {
@@ -158,8 +113,7 @@ export const BOOKS = [
     authors: "John Locke",
     pdfUrl:
       "https://www.xr.pro.br/if/locke-segundo_tratado_sobre_o_governo.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/81fFpc97I8L._UF1000,1000_QL80_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/8718168-M.jpg",
     disciplines: ["Redação"],
   },
   {
@@ -167,8 +121,7 @@ export const BOOKS = [
     authors: "Arthur Conan Doyle",
     pdfUrl:
       "https://kbook.com.br/wp-content/files_mf/asaventurasdesherlockholmes.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfkAObglmOekTDjzRznQdgQ5aqDgUtU7UxZdZ-6CQd7w&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/13469859-M.jpg",
     disciplines: ["Matemática"],
   },
 
@@ -177,8 +130,7 @@ export const BOOKS = [
     authors: "Júlio Verne",
     pdfUrl:
       "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_ba904ddcc828485b865fbd0a48a5ac62.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfgOcII_kewllkBiVyzFRYvFGEYEB0Em4fjr-GRzPMFw&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/12372749-M.jpg",
     disciplines: ["Matemática"],
   },
   {
@@ -204,8 +156,7 @@ export const BOOKS = [
     authors: "Júlio Verne",
     pdfUrl:
       "https://archivepublicdomain.com/files/2025/03/Da-Terra-a-Lua-Julio-Verne.pdf?t=334a7f20471ee414e3",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfHLlEZJo2Y0AabrjLVn07QK2_hQDA_lR_qPq1Pn2vTQ&s=10",
+    imageUrl: "https://covers.openlibrary.org/b/id/12390946-M.jpg",
     disciplines: ["Matemática"],
   },
   {
@@ -213,8 +164,7 @@ export const BOOKS = [
     authors: "Manuel Antônio de Almeida",
     pdfUrl:
       "https://fundar.org.br/wp-content/uploads/2021/06/memorias-de-um-sargento-de-milicias.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/71MebPfNmQL._AC_UF1000,1000_QL80_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/14824687-M.jpg",
     disciplines: ["Geografia"],
   },
   {
@@ -223,8 +173,7 @@ export const BOOKS = [
     disciplines: ["Geografia"],
     pdfUrl:
       "https://archivepublicdomain.com/files/2025/03/a_volta_ao_mundo_em_80_dias_-_julio_verne.pdf?t=c2a25085a6df78cd834c79c7d89d1d1eaff52893697ea5bc4f1224df032d06e9",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/51891EKxfNL._SY445_SX342_ML2_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/13689116-M.jpg",
   },
 
   {
@@ -233,8 +182,7 @@ export const BOOKS = [
     disciplines: ["Geografia"],
     pdfUrl:
       "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_0f5053815e234cc997dbbe6393ba925e.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/41O6ap88L9L._SY445_SX342_ML2_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/4920763-M.jpg",
   },
 
   {
@@ -246,24 +194,13 @@ export const BOOKS = [
     imageUrl:
       "https://s4.static.brasilescola.uol.com.br/be/2023/07/ilustracao-de-um-grupo-de-indigenas-na-capa-do-livro-o-guarani-de-jose-de-alencar.jpg",
   },
-
-  {
-    title: "Triste Fim de Policarpo Quaresma",
-    authors: "Lima Barreto",
-    disciplines: ["História"],
-    pdfUrl:
-      "https://camara-gov-br.usrfiles.com/ugd/5ca0e9_ebda356b2f4742f3bf3325afd9e44363.pdf",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2juuXHASlvgZ1hOj86wydecCjBl6dlEdS2Q&s",
-  },
-
   {
     title: "Os Miseráveis",
     authors: "Victor Hugo",
     disciplines: ["História"],
     pdfUrl:
       "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_877ef06ea4304c21933dff36fbb87935.pdf",
-    imageUrl: "https://m.media-amazon.com/images/I/71L28YvPobL._SY522_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/12919122-M.jpg",
   },
 
   {
@@ -271,17 +208,6 @@ export const BOOKS = [
     authors: "Thomas More",
     disciplines: ["História"],
     pdfUrl: "https://www.ebooksbrasil.org/adobeebook/utopia.pdf",
-    imageUrl:
-      "https://m.media-amazon.com/images/I/91ZnuoJ-I0L._UF1000,1000_QL80_.jpg",
+    imageUrl: "https://covers.openlibrary.org/b/id/13182727-M.jpg",
   },
-
-  {
-    title: "Memórias de um Sargento de Milícias",
-    authors: "Manuel Antônio de Almeida",
-    disciplines: ["História"],
-    pdfUrl:
-      "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_5ad320b0fed546a599ca5d2eb4a4cf1f.pdf",
-    imageUrl:
-      "https://www.baixelivros.com.br/media/2020/03/memorias-de-um-sargento.jpg",
-  },
-];
+]
